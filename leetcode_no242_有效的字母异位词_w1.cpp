0e8afde9a224080592f737993fd729274684608f
BestBlade <68151785+BestBlade@users.noreply.github.com>
2020-11-22 22:39:38 +0800

    Add files via upload

---
+++ leetcode_no242_有效的字母异位词_w1.cpp
@@ -0,0 +1,72 @@
+﻿/* ------------------------------------------------------------------|
+给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+
+示例 1:
+
+输入: s = "anagram", t = "nagaram"
+输出: true
+示例 2:
+
+输入: s = "rat", t = "car"
+输出: false
+说明:
+你可以假设字符串只包含小写字母。
+
+进阶:
+如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/valid-anagram
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	
+*
+*	执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+*	内存消耗：7.7 MB, 在所有 C++ 提交中击败了7.09%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+bool isAnagram(string s, string t) {
+    if (s.size() != t.size()) {
+        return false;
+    }
+
+    int scnt[26] = { 0 };
+    int cnt = 0;
+    for (char& x: s) {
+        int k = x - 'a';
+        scnt[k]++;
+    }
+
+    for (char& x: t) {
+        int k = x - 'a';
+        scnt[k]--;
+        if (scnt[k] < 0) {
+            return false;
+        }
+    }
+    return true;
+}
